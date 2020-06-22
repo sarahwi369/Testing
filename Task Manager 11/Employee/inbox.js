13b420b0305f499713dd8d0ba90bb436e12a13f4
Sarah <swill369@uncc.edu>
2020-06-22 04:36:33 +0000

    Employee Message

--- Task Manager 11/Employee/inbox.js
+++ Task Manager 11/Employee/inbox.js
@@ -1,5 +1,13 @@
-function message() {
-	window.location.href='./message.html';
+function message_mexican() {
+        window.location.href='./message_mexican.html';
+}
+
+function message_hey() {
+        window.location.href='./message_hey.html';
+}
+
+function message_wallet() {
+        window.location.href='./message_wallet.html';
 }
 
 function create() {
